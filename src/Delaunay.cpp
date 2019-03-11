b678b558b8c3a288f65388c16f127313c5bd3e1b
Joffrey D <32282295+InflamedBanana@users.noreply.github.com>
2019-03-11 22:58:45 +0100

    Implemented GetDividingPlane, GetCircumCenter, GetConvexHull, Triangulate & MakeSimplex functions

--- src/Delaunay.cpp
+++ src/Delaunay.cpp
@@ -3,8 +3,11 @@
 #include <utility>
 #include "Math3D.h"
 #include <algorithm>
+#include <stack>
 
 using namespace std;
+
+#define SQRT(x) (x*x)
 
 namespace Delaunay
 {
@@ -16,24 +19,30 @@ namespace Delaunay
         {
             for (auto &point : pointSet)
             {
-                cout << "point : " << point << " - " << wall.position << " = " << (point - wall.position) << endl;
                 if (Dot(point - wall.position, wall.position + wall.normal) >= .0f)
-                {
-                    p2.push_back(point);
-                }
+                    p2.push_back(point);                
                 else
                     p1.push_back(point);
             }
         }
 
-        /* Plane GetDividingPlane(const vector<Vect3<float>> &pointSet)
-         {*/
-         //Create a line
-         //Projects all other points on line ( ignore y )
-         //order them
-         //get center point
-         //return plane 
-     //}
+        Plane GetDividingPlane(const vector<Vect3<float>> &pointSet)
+        {
+            float lowestX(99999), biggestX(-99999), lowestZ(99999), biggestZ(-99999);
+
+            for (const auto &point : pointSet)
+            {
+                if (point.x < lowestX) lowestX = point.x;
+                else if (point.x > biggestX) biggestX = point.x;
+                if (point.z < lowestZ) lowestZ = point.z;
+                else if (point.z > biggestZ) biggestZ = point.z;
+            }
+
+            if (biggestX - lowestX >= biggestZ - lowestZ)
+                return Plane(Vect3<float>(1.0f, .0f, .0f), Vect3<float>((biggestX + lowestX) / 2.f, .0f, .0f));
+            else
+                return Plane(Vect3<float>(.0f, .0f, 1.0f), Vect3<float>(.0f, .0f, (biggestZ + lowestZ) / 2.f));
+        }
 
 
         float DistanceToPlane(const Vect3<float> &_point, const Plane &_plane)
@@ -44,6 +53,15 @@ namespace Delaunay
         float GetCircumCircleRadius(const float _distAB, const float _distBC, const float _distAC)
         {
             return ((_distAB * _distBC * _distAC) / sqrt(((_distAB + _distBC + _distAC)*(_distBC + _distAC - _distAB)*(_distAC + _distAB - _distBC)*(_distAB + _distBC - _distAC))));
+        }
+
+        Vect3<float> GetCircumCenter(const Vect3<float> &_A, const Vect3<float> &_B, const Vect3<float> &_C)
+        {
+            float a = (_B - _C).GetSqrMagnitude();
+            float b = (_A - _C).GetSqrMagnitude();
+            float c = (_B - _A).GetSqrMagnitude();
+
+            return (a * (b + c - a) * _A + b * (c + a - b) * _B + c * (a + b - c) * _C) / (a * (b + c - a) + b * (c + a - b) + c * (a + b - c));
         }
 
         Triangle MakeFirstSimplex(const std::vector<Vect3<float>> &pointSet, const Plane &wall)
@@ -97,56 +115,181 @@ namespace Delaunay
                 }
             }
 
-            return Triangle(a, b, c);
+            return Triangle(new Edge(a, b, c), new Edge(b, c, a), new Edge(c, a, b));
+        }
+
+        //> 0 if counterclockwise, < 0 if clockwise & == 0 is collinear
+        int CounterClockWise(const Vect3<float> &_nextToTopStack, const Vect3<float> &_topStack, const Vect3<float> _point)
+        {
+            auto cross = (_topStack.x - _nextToTopStack.x)*(_point.z - _nextToTopStack.z) - (_topStack.z - _nextToTopStack.z)*(_point.x - _nextToTopStack.x);
+            return cross > 0 ? 1 : (cross < 0 ? -1 : 0);
+        }
+
+        vector<Vect3<float>>* GetConvexHull(vector<Vect3<float>> _pointSet)
+        {
+            auto lowestZCoord = min_element(_pointSet.begin(), _pointSet.end(), [](const Vect3<float>& a, const Vect3<float>& b) { return (a.z < b.z || (a.z == b.z && a.x < b.x)); });
+            auto p = *lowestZCoord;
+            _pointSet.erase(lowestZCoord);
+
+            sort(_pointSet.begin(), _pointSet.end(), [&p](const Vect3<float>& a, const Vect3<float>& b)
+            {
+                Vect3<float> xAxis(1.0f, .0f, .0f);
+                auto vectA = a - p;
+                auto vectB = b - p;
+                float cosA = Dot(vectA, xAxis) / vectA.GetMagnitude();
+                float cosB = Dot(vectB, xAxis) / vectB.GetMagnitude();
+                return cosA > cosB;
+            });
+            //Very bad.
+            for (int i = _pointSet.size() - 1; i > 0; --i)
+            {
+                Vect3<float> xAxis(1.0f, .0f, .0f);
+                auto vectA = _pointSet[i] - p;
+                auto vectB = _pointSet[i - 1] - p;
+                float cosA = Dot(vectA, xAxis) / vectA.GetMagnitude();
+                float cosB = Dot(vectB, xAxis) / vectB.GetMagnitude();
+
+                if (cosA == cosB)
+                {
+                    if (vectA.GetMagnitude() > vectB.GetMagnitude())
+                        _pointSet.erase(_pointSet.begin() + i - 1);
+                    else
+                        _pointSet.erase(_pointSet.begin() + i);
+                }
+            }
+
+            vector<Vect3<float>>* convexHull = new vector<Vect3<float>>();
+            convexHull->push_back(p);
+
+            for (const auto &point : _pointSet)
+            {
+                while (convexHull->size() > 1 && CounterClockWise((*convexHull)[convexHull->size() - 2], convexHull->back(), point) < 0)
+                    convexHull->pop_back();
+
+                convexHull->push_back(point);
+            }
+
+            return convexHull;
+        }
+
+        void MakeSimplex(Edge &_edge, const PointSet &_pointSet, Triangle *&_triangle, const vector<Vect3<float>> *_convexHull)
+        {
+            if (find((*_convexHull).begin(), (*_convexHull).end(), _pointSet[_edge.a]) != (*_convexHull).end() &&
+                find((*_convexHull).begin(), (*_convexHull).end(), _pointSet[_edge.b]) != (*_convexHull).end())
+            {
+                _triangle = nullptr;
+                return;
+            }
+
+            Vect3<float> edgeCenter = (_pointSet[_edge.a] + _pointSet[_edge.b]) / 2.0f;
+            Vect3<float> planeNormal = GetCircumCenter(_pointSet[_edge.a], _pointSet[_edge.b], _pointSet[_edge.oppositePoint]) - edgeCenter;
+            Plane halfPlane(planeNormal, edgeCenter);
+
+            bool cSide = Dot(_pointSet[_edge.oppositePoint] - halfPlane.position, halfPlane.position + halfPlane.normal) >= .0f;
+
+            int pointIndex = -1;
+            float circumcircleRadius = .0f;
+            
+            float distAB = (_pointSet[_edge.b] - _pointSet[_edge.a]).GetMagnitude();
+
+            for (int i = 0; i < _pointSet.size(); ++i)
+            {
+                bool halfPlanePos = Dot(_pointSet[i] - halfPlane.position, halfPlane.position + halfPlane.normal) >= .0f;
+
+                if (i == _edge.a || i == _edge.b || _edge.oppositePoint == i ||
+                    cSide && halfPlanePos ||
+                    !cSide && !halfPlanePos) // check if point is in HalfSpace.
+                    continue;
+
+                float radius = 
+                    GetCircumCircleRadius(sqrt(distAB), 
+                    (_pointSet[i] - _pointSet[_edge.b]).GetMagnitude(), 
+                    (_pointSet[i] - _pointSet[_edge.a]).GetMagnitude());
+
+                if (radius < circumcircleRadius || pointIndex == -1)
+                {
+                    pointIndex = i;
+                    circumcircleRadius = radius;
+                }
+            }
+            
+            _triangle = new Triangle(&_edge, new Edge(_edge.a, pointIndex, _edge.b), new Edge(_edge.b, pointIndex, _edge.a));
+        }
+
+        void AddEdgeToAFLs(const PointSet &_vertices, const Edge &_edge, const Plane &_wall, const PointSet &_p1, vector<Edge> &_aflw, vector<Edge> &_afl1, vector<Edge> &_afl2 )
+        {
+            if (Math3D::RayPlaneIntersection(_vertices[_edge.a], _vertices[_edge.b] - _vertices[_edge.a], _wall.position, _wall.normal))
+            {
+                _aflw.push_back(_edge);
+            }
+            else if (any_of(_p1.begin(), _p1.end(), [&_edge, &_vertices](const Vect3<float> &vertice) {return vertice == _vertices[_edge.a]; }))
+            {
+                _afl1.push_back(_edge);
+            }
+            else
+                _afl2.push_back(_edge);
         }
     }
 
-    std::vector<Triangle> Triangulate(const PointSet &_vertices, std::vector<Edge> *_afl)
+    std::vector<Triangle> Triangulate(const PointSet &_pointSet, std::vector<Edge> *_afl, std::vector<Vect3<float>> *_convexHull)
     {
         vector<Edge> aflw, afl1, afl2;
-
-        PointSet p1, p2;
-        Plane wall(Vect3<float>(1.f, .0f, 0.f), Vect3<float>(.0f, .0f, .0f)); // calculate wall
-
         vector<Triangle> triangles;
-        //new list /\
+        PointSet p1, p2;
 
-        PartitionPointSet(_vertices, wall, p1, p2);
+        if (_convexHull == nullptr)
+        {
+            _convexHull = GetConvexHull(_pointSet);
+        }
+
+        Plane wall = GetDividingPlane(_pointSet);
+
+        PartitionPointSet(_pointSet, wall, p1, p2);
 
         if (_afl == nullptr)
         {
-            Triangle t = MakeFirstSimplex(_vertices, wall);
-            _afl->push_back(Edge(_vertices[t.a], _vertices[t.b]));
-            _afl->push_back(Edge(_vertices[t.b], _vertices[t.c]));
-            _afl->push_back(Edge(_vertices[t.c], _vertices[t.a]));
+            _afl = new vector<Edge>();
+            Triangle t = MakeFirstSimplex(_pointSet, wall);
+            _afl->push_back(*t.a);
+            _afl->push_back(*t.b);
+            _afl->push_back(*t.c);
             triangles.push_back(t);
         }
 
         for (const auto& edge : *_afl)
         {
-            if (Math3D::RayPlaneIntersection(edge.a, edge.b - edge.a, wall.position, wall.normal))
-            {
-                aflw.push_back(edge);
-            }
-            else if (any_of(p1.begin(), p1.end(), [edge](const Vect3<float> &vertice) {return vertice == edge.a; }))
-            {
-                afl1.push_back(edge);
-            }
-            else
-                afl2.push_back(edge);
+            AddEdgeToAFLs(_pointSet, edge, wall, p1, aflw, afl1, afl2);
         }
 
         while (aflw.size() > 0)
         {
-            //get last edge
-            //MakeSimplex(edge, _vertices)
+            auto edge = aflw.back();
+            aflw.pop_back();
+            
+            Triangle *triangle = nullptr;
+            MakeSimplex(edge, _pointSet, triangle, _convexHull);
+
+            if (triangle != nullptr)
+            {
+                triangles.push_back(*triangle);
+
+                AddEdgeToAFLs(_pointSet, *triangle->b , wall, p1, aflw, afl1, afl2);
+                AddEdgeToAFLs(_pointSet, *triangle->c, wall, p1, aflw, afl1, afl2);
+            }
+        }
+        
+        if (afl1.size() > 0)
+        {
+            auto afl1Triangles = Triangulate(p1, &afl1);
+            triangles.insert(triangles.end(), afl1Triangles.begin(), afl1Triangles.end());
+        }
+        if (afl2.size() > 0)
+        {
+            auto afl2Triangles = Triangulate(p2, &afl2);
+            triangles.insert(triangles.end(), afl2Triangles.begin(), afl2Triangles.end());
         }
 
-
-
-        //return List/\
-
-        return vector<Triangle>();
+        return triangles;
     }
 
 } // namespace Delaunay
