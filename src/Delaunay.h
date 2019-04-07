d3bafe5db315261632663c61631a759bdbf067b4
Joffrey D <32282295+InflamedBanana@users.noreply.github.com>
2019-04-07 22:22:06 +0200

    edge == & != operators

--- src/Delaunay.h
+++ src/Delaunay.h
@@ -12,22 +12,12 @@ namespace Delaunay
         short oppositePoint;
         Edge(const int _a, const int _b, const int _opposite = -1) : a(_a), b(_b), oppositePoint(_opposite) {}
 
-       /* friend bool operator==(const Edge &_a, const Edge &_b);
-        friend bool operator!=(const Edge &_a, const Edge &_b);*/
-
-        bool operator==(const Edge &_b) { return a == _b.a && b == _b.b || a == _b.b && b == _b.a; }
-        bool operator!=(const Edge &_b) { return !(*this == _b); }
+        bool operator==(const Edge &) const;
+        bool operator!=(const Edge &) const;
     };
 
-   /* bool operator==(const Edge &_a, const Edge &_b) { return _a.a == _b.a && _a.b == _b.b; }
-    bool operator!=(const Edge &_a, const Edge &_b) { return !(_a == _b); }*/
-
-    struct Plane
-    {
-        Vect3<float> normal;
-        Vect3<float> position;
-        Plane(const Vect3<float> &_normal, const Vect3<float> &_position) : normal(_normal), position(_position) {}
-    };
+    inline bool Edge::operator==(const Edge &_b) const { return a == _b.a && b == _b.b || a == _b.b && b == _b.a; }
+    inline bool Edge::operator!=(const Edge &_b) const { return !(*this == _b); }
 
     struct Triangle
     {
